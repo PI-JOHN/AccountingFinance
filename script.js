468add0768a3ec70cd554f811420f4ca4136d884
JOHN <boriskashtanov80@gmail.com>
2020-06-25 16:31:20 +0400

    add method daily budget

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 'use strict';
 
-let arr = ['plum.png', 'apple.png'];
-console.log(arr[0]);
+let money,
+    time;
+money = +prompt("Your monthly budget","");
+time = prompt("Enter a date in the format: YYYY-MM-DD","");
+let ask11 = prompt("Введите обязательную статью расходов в этом месяце","");
+let ask12 = +prompt("Во сколько обойдется?","");
+let ask21 = prompt("Введите обязательную статью расходов в этом месяце","");
+let ask22 = +prompt("Во сколько обойдется","");
 
-let answer = prompt("Есть ли вам 18 лет?", "");
-console.log(answer);
+let arr = [];
 
+let appData = {
+    budget: [money],
+    timeData: [time],
+    expenses: {
+        [ask11]: [ask12],
+        [ask21]: [ask22]
+    },
+    optionalExpenses: {
 
+    },
+    income: [arr],
+    saving: false
+};
+
+let dailyBudget = (money-(ask12 + ask22))/30;
+
+let sum = alert("Ваш бюджет на один день: "+dailyBudget);
+console.log( appData);
